44a0885f3c46312b9bd70a95d1ec5a5379ec06ba
Martin Otero <martin.otero@digitalcrackers.com>
2024-02-09 16:46:43 -0300

    Fly 1

--- jatte/settings.py
+++ jatte/settings.py
@@ -45,14 +45,13 @@ CHANNEL_LAYERS = {
 # Application definition
 
 INSTALLED_APPS = [
-
-    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'daphne',
     'account',
     'chat',
     'core',
